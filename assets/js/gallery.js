7486dd2e5d452f158089464ba52397070b1e8994
willy0483 <willyjensen251@gmail.com>
2024-04-26 12:30:27 +0200

    Galleri-karusel

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,60 @@
 // Din cool code here
+
+
+
+
+// array til billeder
+let imageArray = [
+  "assets/img/elephant.jpg",
+  "assets/img/great-white.jpg",
+  "assets/img/koala.jpg",
+  "assets/img/smithi.jpg",
+  "assets/img/tiger.jpg",
+];
+
+let currentImage = 0
+
+// galleri billede fra dom
+let myGalleriImageElement = document.getElementById('galleryImage')
+
+
+
+    // fremad knap fra dom
+let myForwardButton = document.getElementById('forward')
+myForwardButton.addEventListener('click',(e)=>{
+
+    currentImage = currentImage + 1
+
+    if (currentImage == imageArray.length) {
+        currentImage = 0
+    }
+    changeImage()
+
+})
+
+
+    // bagud knap fra dom
+let myBackButton = document.getElementById('back')
+
+myBackButton.addEventListener('click',(e)=>{
+
+    currentImage = currentImage - 1
+
+    if (currentImage < 0) {
+        currentImage = 4
+    }
+
+    changeImage()
+
+})
+
+
+function changeImage(){
+    console.log(imageArray[currentImage])
+    myGalleriImageElement.src = imageArray[currentImage]
+
+}
+
+
+
+myGalleriImageElement.src = imageArray[currentImage]
